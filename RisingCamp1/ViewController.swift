29abc972d46f36886fc9976326c38017c9b5a282
meltsplit <seokwoo2000@naver.com>
2022-06-06 21:17:52 +0900

    Initial Commit

---
+++ RisingCamp1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RisingCamp1
+//
+//  Created by 장석우 on 2022/06/06.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
