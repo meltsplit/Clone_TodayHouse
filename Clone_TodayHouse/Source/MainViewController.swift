794ec7fe40f971c128ce6ba9b9cb42c5952d3d91
meltsplit <seokwoo2000@naver.com>
2022-06-08 02:05:46 +0900

    [Add] : 프로젝트 초기 세팅

---
+++ Clone_TodayHouse/Source/MainViewController.swift
@@ -0,0 +1,22 @@
+//
+//  MainViewController.swift
+//  RisingCamp1
+//
+//  Created by 장석우 on 2022/06/06.
+//
+
+import Foundation
+import UIKit
+class MainViewController : UIViewController{
+    
+    @IBOutlet weak var mainNavigationBar: UINavigationItem!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        navigationBarSetting()
+    }
+    
+    private func navigationBarSetting(){
+        
+    }
+}
